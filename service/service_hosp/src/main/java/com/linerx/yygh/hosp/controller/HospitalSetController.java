61733ac7e4d11df58be03d6dda88ba288350aaee
jinchenglai <yfbjcl@hanyuanzhihui.com>
2021-03-30 09:26:15 +0800

    Vo类导入及分页查询实现

--- service/service_hosp/src/main/java/com/linerx/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/linerx/yygh/hosp/controller/HospitalSetController.java
@@ -1,15 +1,20 @@
 package com.linerx.yygh.hosp.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.api.R;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.linerx.yygh.common.result.Result;
 import com.linerx.yygh.hosp.service.HospitalSetService;
 import com.linerx.yygh.model.hosp.HospitalSet;
+import com.linerx.yygh.vo.hosp.HospitalQueryVo;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Wrapper;
 import java.util.List;
 
 @RestController
@@ -34,5 +39,23 @@ public class HospitalSetController {
         }else {
             return Result.fail();
         }
+    }
+    @ApiOperation(value = "分页查询")
+    @PostMapping("getPageHospital/{current}/{limit}")
+    private Result getPageHospital(@PathVariable Long current,@PathVariable Long limit,@RequestBody HospitalQueryVo hospitalQueryVo){
+        Page<HospitalSet> page = new Page<>(current,limit);
+        //构造查询条件
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper();
+        String hosname = hospitalQueryVo.getHosname();
+        String hoscode = hospitalQueryVo.getHoscode();
+        if (StringUtils.isNotBlank(hosname)){
+            wrapper.like("hosname",hospitalQueryVo.getHosname());
+        }
+        if (StringUtils.isNotBlank(hoscode)){
+            wrapper.eq("hoscode",hospitalQueryVo.getHoscode());
+        }
+        Page<HospitalSet> hospitalSetPage = hospitalSetService.page(page,wrapper);
+        return Result.ok(hospitalSetPage);
+
     }
 }
